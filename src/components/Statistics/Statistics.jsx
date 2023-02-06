f4a958a5a25796aa85b96e3c4edbfbcdc65e4435
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-07 00:15:55 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,29 @@
-{/* <section class="statistics">
-  <h2 class="title">Upload stats</h2>
+import PropTypes from 'prop-types';
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">{title}</h2>
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section> */}
+      <ul class="stat-list">{stats.map(({ id, label, percentage }) => {
+        return (
+        <li class="item" key={id}>
+          <span class="label">{label}</span>
+          <span class="percentage">{percentage}%</span>
+        </li>
+        )
+      };
+        );
+      };
+      </ul>
+    </section>
+  )
+};
 
-// import data from '/path/to/data.json';
-
-// <Statistics title="Upload stats" stats={data} />
-// <Statistics stats={data} />
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired
+  })
+}
