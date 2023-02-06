f8ecb810f99ae129b4cad6cc28c0d07a7d39ca8a
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-07 01:20:46 +0200

    added friends folder

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,17 @@
-{/* <ul class="friend-list">
-  <!-- Довільна кіл-сть FriendListItem -->
-</ul> */}
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({friends}) => {
+  return (
+    <ul class="friend-list">{friends.map(({avatar, name, isOnline, id}))=>{
+      return <FriendListItem key={id} avatar={avatar} name={ name} isOnline={isOnline} />
+    }}
+    </ul>
+  )
+}
+FriendList.propTypes = {
+  avatar: PropTypes.string.isRequired,,
+  name: PropTypes.string.isRequired,,
+  isOnline: PropTypes.bool.isRequired,,
+  id: PropTypes.number.isRequired,
+}
