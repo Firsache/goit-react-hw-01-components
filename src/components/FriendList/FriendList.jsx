36d70a9691693863dc2f297b555208deb5992660
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-07 11:12:24 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
 
-export const FriendList = ({friends}) => {
+export const FriendList = (friends) => {
   return (
-    <ul class="friend-list">{friends.map(({avatar, name, isOnline, id}))=>{
-      return <FriendListItem key={id} avatar={avatar} name={ name} isOnline={isOnline} />
-    }}
+    <ul class="friend-list">{friends.map(({ avatar, name, isOnline, id }) => {
+      return (<FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />)
+    })}
     </ul>
   )
 }
 FriendList.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired
+  friends: PropTypes.array.isRequired
+  // avatar: PropTypes.string.isRequired,
+  // name: PropTypes.string.isRequired,
+  // isOnline: PropTypes.bool.isRequired,
+  // id: PropTypes.number.isRequired
 }
