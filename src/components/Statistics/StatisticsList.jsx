a5abe199614ad634d881a4824bb74bb64dca5bc1
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-08 12:36:40 +0200

    added styles to Statisctics

--- src/components/Statistics/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import { StatsList, StatsItem } from './Statistics.styled';
+import { generateRandomColor } from 'utils/RandomColor';
 
 export const StatisticsList = ({ stats }) => {
-    return (
-      <StatsList>{stats.map(({ id, label, percentage }) => {
+  return (
+    <StatsList>{stats.map(({ id, label, percentage }) => {
+      const randomColor = generateRandomColor();
         return (
-        <StatsItem key={id}>
+        <StatsItem key={id} backgroundColor={randomColor}>
           <span className="label">{label}</span>
           <span className="percentage">{percentage}%</span>
         </StatsItem>
